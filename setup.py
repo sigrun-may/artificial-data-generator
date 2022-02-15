f91bd6b9317f26ed4d3dc9e13ddb1a07b48deecf
sigrun.may <sigrun.may@googlemail.com>
2022-02-15 11:00:41 +0100

    update copyright

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 # Copyright (c) 2022 Sigrun May,
-# Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
-# Copyright (c) 2022 Sigrun May,
 # Ostfalia Hochschule für angewandte Wissenschaften
 #
 # This software is distributed under the terms of the MIT license
