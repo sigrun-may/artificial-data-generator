0decf36df90c5c5aec14ba20600ea6ace409a551
Sigrun May <sigrun.may@gmail.com>
2023-03-02 17:51:43 +0100

    Bump to version 0.0.1rc9

--- src/__init__.py
+++ src/__init__.py
@@ -11,7 +11,7 @@
 from src.artificial_data_generator import generate_artificial_classification_data
 
 
-__version__ = "0.0.1rc8"
+__version__ = "0.0.1rc9"
 
 __all__ = [
     "generate_artificial_classification_data",
